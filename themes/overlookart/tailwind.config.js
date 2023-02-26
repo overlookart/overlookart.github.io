a8cf2d49b1504bc6100128b16265dbdc69448faa
丫丫的刀了 <598823426@qq.com>
2023-02-26 14:02:39 +0800

    bg image

--- themes/overlookart/tailwind.config.js
+++ themes/overlookart/tailwind.config.js
@@ -3,7 +3,11 @@ const plugin = require('tailwindcss/plugin')
 module.exports = {
     content: ['content/**/*.md', 'layouts/**/*.html'],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage:{
+                "sitebg":"url('https://source.unsplash.com/collection/399194')"
+            }
+        },
     },
     plugins: [
         plugin((addUtilities, addComponents, e, prefix, config) => {
