ceb0d9e9166fbe90b464af3f4b4772c1f4e0860e
丫丫的刀了 <598823426@qq.com>
2023-12-17 02:29:13 +0800

    覆盖 typography 插件中 markdown 生成的 code 标签样式： 取消 before, after 伪类内容，及增加文字颜色 hugo 的 render-heading.html 中的样式抽取成单独的scss文件

--- themes/overlookart/tailwind.config.js
+++ themes/overlookart/tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 const plugin = require('tailwindcss/plugin')
 module.exports = {
+    corePlugins: {
+        preflight: true,
+    },
     content: ['content/**/*.md', 'layouts/**/*.html'],
     theme: {
         extend: {
             backgroundImage:{
                 "sitebg":"url('https://source.unsplash.com/random/1280x800?simple,color,bright')"
+            },
+            // 自定义 typography 插件样式
+            typography: {
+                DEFAULT: {
+                    css: {
+                        code: {
+                            color: 'rgb(239, 112, 96)'
+                        },
+                        'code::before': {
+                            content: ''
+                        },
+                        'code::after': {
+                            content: ''
+                        }
+                    }
+                }
             }
         },
     },
