e4429c8414df63e7553a78d308256b7b2539cd02
丫丫的刀了 <598823426@qq.com>
2023-12-25 08:30:34 +0800

    update

--- themes/overlookart/assets/js/main.js
+++ themes/overlookart/assets/js/main.js
@@ -1,2 +1,28 @@
 import * as params from '@params';
-console.debug(params.api);
+import lax from './modules/lax'
+console.debug('加载主脚本');
+
+window.onload = function () {
+    lax.init()
+    console.debug('初始化 lax');
+    
+    // 添加动画驱动
+    lax.addDriver('scrollY', function () {
+            return window.scrollY
+        },
+        inertiaEnabled = true
+    )
+
+    // 向元素添加动画绑定
+    lax.addElements('.selector', {
+        scrollY: {
+            translateX: [
+                [0,  "pageHeight"],
+                [0, 'pageWidth'],
+                {
+                    inertia: 10
+                }
+            ]
+        }
+    })
+}
