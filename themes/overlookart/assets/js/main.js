e439083969c39ff7d5b1c1384749e89b0b311c80
丫丫的刀了 <598823426@qq.com>
2023-12-28 23:37:11 +0800

    window 事件

--- themes/overlookart/assets/js/main.js
+++ themes/overlookart/assets/js/main.js
@@ -2,27 +2,48 @@ import * as params from '@params';
 import lax from './modules/lax'
 console.debug('加载主脚本');
 
-window.onload = function () {
-    lax.init()
-    console.debug('初始化 lax');
-    
-    // 添加动画驱动
-    lax.addDriver('scrollY', function () {
-            return window.scrollY
-        },
-        inertiaEnabled = true
-    )
-
-    // 向元素添加动画绑定
-    lax.addElements('.selector', {
-        scrollY: {
-            translateX: [
-                [0,  "pageHeight"],
-                [0, 'pageWidth'],
-                {
-                    inertia: 10
-                }
-            ]
-        }
-    })
+window.onload = () => {
+    console.debug('整个页面及所有依赖资源如样式表和图片都已完成');
 }
+
+window.oncopy = (event) => {
+    console.debug('复制!', event);
+}
+
+window.onpaste = (event) => {
+    console.debug('粘贴!', event);
+}
+
+window.oncut = (event) => {
+    console.debug('剪切!', event);
+}
+
+window.ononline = (event) => {
+    console.debug('在线!', event);
+}
+window.onoffline = (event) => {
+    console.debug('离线!', event);
+}
+
+window.onhashchange = (event) => {
+    console.debug('URL改变!', event);
+}
+
+window.onpopstate = (event) => {
+    console.debug('导航!', event)
+}
+
+window.onpageshow = (event) => {
+    console.debug('进入页面!', event);
+}
+window.onpagehide = (event) => {
+    console.debug('离开页面!', event);
+}
+window.onbeforeunload = (event) => {
+    console.debug('卸载页面!', event);
+}
+
+window.onresize = (event) => {
+    console.debug('窗口大小调整!', event);
+}
+
