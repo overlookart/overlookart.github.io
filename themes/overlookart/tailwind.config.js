d67264dfe488073756a179fb4df14d4db983fa90
丫丫的刀了 <598823426@qq.com>
2024-09-17 21:13:59 +0800

    修复文章页内容在小窗口溢出的问题

--- themes/overlookart/tailwind.config.js
+++ themes/overlookart/tailwind.config.js
@@ -24,7 +24,6 @@ module.exports = {
             typography: {
                 DEFAULT: {
                     css: {
-                        'max-width': '90ch',
                         h2: {
                             'margin-top': 'unset',
                             'margin-bottom': 'unset'
