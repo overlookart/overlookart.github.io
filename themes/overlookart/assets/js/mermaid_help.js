cc198882b27f5e79fc4580592da8d2b589acf97b
丫丫的刀了 <598823426@qq.com>
2024-12-03 19:36:07 +0800

    npm 管理 mermaid

---
+++ themes/overlookart/assets/js/mermaid_help.js
@@ -0,0 +1,10 @@
+import mermaid from "mermaid";
+mermaid.initialize({ 
+    startOnLoad: true,
+    // 配置主题
+    theme: 'base',
+    // 配置日志等级
+    logLevel: 'info',
+    // 配置安全等级
+    securityLevel: 'loose'
+});
